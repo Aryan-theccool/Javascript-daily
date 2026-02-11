6b1912f67213e35cd9370ab36aba1364d0a94831
Aryan singh bhadoria <aryanbhadoria100@gmail.com>
2026-02-11 23:38:30 +0530

    upload and submit

--- events and listen/choosefile.js
+++ events and listen/choosefile.js
@@ -1,1 +1,11 @@
-document.querySelector("#btn")
+let btn = document.querySelector("#btn");
+let file=document.querySelector("#fileinput");
+btn.addEventListener("click",function(){
+  file.click();
+});
+file.addEventListener("change",function(dets){
+  let filee=dets.target.files[0];
+  if(filee){
+    btn.textContent=filee.name;
+  }
+})
