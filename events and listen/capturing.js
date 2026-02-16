b7cb3c65fbdefb838e9ed096f199884512949dfa
Aryan singh bhadoria <aryanbhadoria100@gmail.com>
2026-02-16 16:19:40 +0530

    capturing

---
+++ events and listen/capturing.js
@@ -0,0 +1,17 @@
+
+let button=document.querySelector("#d");
+button.addEventListener("click",function(val){
+console.log("button clicked");
+});
+let c=document.querySelector("#c");
+c.addEventListener("click",function(val){
+console.log("c clicked");
+});
+let b=document.querySelector("#b");
+b.addEventListener("click",function(val){
+console.log("b clicked");
+},true);
+let a=document.querySelector("#a");
+a.addEventListener("click",function(val){
+console.log("a clicked");
+});
