d3f2b52b8f2d831061f3eb05a9159e1dce5dfec4
Aryan singh bhadoria <aryanbhadoria100@gmail.com>
2026-02-18 18:27:14 +0530

    regex done

--- forms and forms validation/form.js
+++ forms and forms validation/form.js
@@ -3,10 +3,15 @@ let form=document.querySelector("form");
 form.addEventListener("submit",function(e){
       e.preventDefault();
 
-  if(nm.value.length<=2){
-document.querySelector("#hide").style.display="initial";
-  }else{
-    document.querySelector("#hide").style.display="none";
-
-  }
-})
+//   if(nm.value.length<=2){
+// document.querySelector("#hide").style.display="initial";
+//   }else{
+//     document.querySelector("#hide").style.display="none";
+// regex
+const regex=/^[A-Za-z]{2,}(?: [A-Za-z]+)*$/;
+regex.test("Aryanthecool");
+const email=/^[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,}$/;
+email.test("aryan@123.com");
+console.log(regex.test("Arya@@ntheeecool"));
+console.log(email.test("aryan@123.com"));
+});
