fb6c65a8775f7406b60f4ac3b36ffdd824d16e70
Aryan singh bhadoria <aryanbhadoria100@gmail.com>
2026-02-18 23:09:44 +0530

    downloaded

--- timers and intervals/scrip.js
+++ timers and intervals/scrip.js
@@ -1,16 +1,32 @@
-let a=setTimeout(function(){
-    console.log("hello");
-},2000);
-clearTimeout(a);
-let b=setInterval(function(){
-    console.log("happp");
-},2000);
-clearInterval(b);
-let count=10;
-let c=setInterval(function(){
-    console.log(count);
-    count--;
-    if(count==0){
-      clearInterval(c);
-    }
-},1000);
+// let a=setTimeout(function(){
+//     console.log("hello");
+// },2000);
+// clearTimeout(a);
+// let b=setInterval(function(){
+//     console.log("happp");
+// },2000);
+// clearInterval(b);
+// let count=10;
+// let c=setInterval(function(){
+//     console.log(count);
+//     count--;
+//     if(count==0){
+//       clearInterval(c);
+//     }
+// },1000);
+
+// suppose 3 sec me 100% karana therefore 1 percent=30ms
+let count =0;
+let progress =document.querySelector(".progress-bar");
+let percent=document.querySelector("#percentText");
+let a=setInterval(function(){
+  count++;
+  progress.style.width=count+"%";
+  percent.textContent=count+"%";
+  if(count==100){
+    clearInterval(a);
+        document.querySelector("h2").textContent="Downloaded";
+
+  }  
+  }
+,30);
