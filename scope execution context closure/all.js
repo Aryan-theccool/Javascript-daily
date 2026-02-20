09d0d4b3ce3c41a6f6d4a0bd7a6e2440f7d1d0c4
Aryan singh bhadoria <aryanbhadoria100@gmail.com>
2026-02-20 22:10:44 +0530

    understanding

---
+++ scope execution context closure/all.js
@@ -0,0 +1,8 @@
+//scope-functional scope,global scope, block
+
+// function scope - ye keval function me use hoga
+//global scope-poore code me use hoga 
+//block - curly me use hoga {}
+
+
+//execution content - js sabse pehle jaise hi aapka pehle function dekhta hai js sabse pehle banta hai execution context, ye ek process hai jo ki do different phase me chalta hai , memory phase and doosre ka naam hai execution phase 
