d386fe49fb09481a3b8242b90cd72bb43f1e32ab
Aryan singh bhadoria <aryanbhadoria100@gmail.com>
2026-03-04 13:24:26 +0530

    half promise

--- async promises/script.js
+++ async promises/script.js
@@ -1,26 +1,48 @@
-function profileLekarAao(username, cb) {
-  // ...
-}
+// function profileLekarAao(username, cb) {
+//   // ...
+// }
 
-function saarePostLekarAao(id, cb) {
-  setTimeout(() => {
-    console.log(`Fetching posts for user id: ${id}`);
-    const posts = [
-      { id: 1, title: "Post 1", likes: 10 },
-      { id: 2, title: "Post 2", likes: 25 },
-      { id: 3, title: "Post 3", likes: 5 }
-    ];
-    cb(posts);
-  }, 1500);
-}
+// function saarePostLekarAao(id, cb) {
+//   setTimeout(() => {
+//     console.log(`Fetching posts for user id: ${id}`);
+//     const posts = [
+//       { id: 1, title: "Post 1", likes: 10 },
+//       { id: 2, title: "Post 2", likes: 25 },
+//       { id: 3, title: "Post 3", likes: 5 }
+//     ];
+//     cb(posts);
+//   }, 1500);
+// }
 
-function savedPostsNikaalo(id, cb) {
-  // ...
-}
+// function savedPostsNikaalo(id, cb) {
+//   // ...
+// }
 
-profileLekarAao("harsh", function (data) {
-  // sa function savedPostsNikaalo (id: any, cb: any): void
-  savedPostsNikaalo(data._id, function (saved) {
-    // ...
-  });
-});
+// profileLekarAao("harsh", function (data) {
+//   // sa function savedPostsNikaalo (id: any, cb: any): void
+//   savedPostsNikaalo(data._id, function (saved) {
+//     // ...
+//   });
+// });
+
+// promises
+//ap ek promise banate ho jo 
+
+
+let pr =new Promise((resolve,reject)=>{
+  setTimeout(()=>{
+    // resolve(345)
+    if(3>5){
+      resolve(345)
+    }
+    else{
+      reject("error")
+    }
+  },4000)
+})
+pr.then(function(data){
+  console.log(data);
+})
+.catch(function(err){
+  console.log(err);
+})
