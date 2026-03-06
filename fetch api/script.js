eb25e8cce86e625ac064ddca426077cdafd6f9fc
Aryan singh bhadoria <aryanbhadoria100@gmail.com>
2026-03-06 23:51:35 +0530

    ramdan

--- fetch api/script.js
+++ fetch api/script.js
@@ -1,14 +1,10 @@
 fetch("https://randomuser.me/api/")
 .then(function(response){
-  return response.json();
-})
-.then(function(data){
+return response.json();
+ })
+ .then(function(data){
   console.log(data);
-  document.body.innerHTML += `<pre>${JSON.stringify(data, null, 2)}</pre>`;
-})
-.catch(function(error){
-  console.log(error);
-})
+ })
 .catch(function(error){
   console.log(error);
 })
