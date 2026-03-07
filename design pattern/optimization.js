2e2bf0c20c11c24253bbed93b8b492d3da5285ee
Aryan singh bhadoria <aryanbhadoria100@gmail.com>
2026-03-07 16:33:19 +0530

    debounce

---
+++ design pattern/optimization.js
@@ -0,0 +1,16 @@
+// document.querySelector('#search').addEventListener('input', (e) => {
+//     console.log(e.target.value);
+// });
+//isme kya hora na hatho hath action perform hora aur ye me nahi chahta 
+
+function debounce(func, delay) {
+    let timeout;
+    return function(...args) {
+        clearTimeout(timeout);
+        timeout = setTimeout(() => func.apply(this, args), delay);
+    };
+}
+
+document.querySelector('#search').addEventListener('input', debounce((e) => {
+    console.log(e.target.value);
+}, 500));
