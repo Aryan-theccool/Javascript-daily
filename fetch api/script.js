8caab288338980900fa14af6e85069dc9e01684d
Aryan singh bhadoria <aryanbhadoria100@gmail.com>
2026-03-07 11:02:42 +0530

    push

--- fetch api/script.js
+++ fetch api/script.js
@@ -1,9 +1,12 @@
-fetch("https://randomuser.me/api/")
+fetch("https://randomuser.me/api/?results=3")
 .then(function(response){
 return response.json();
  })
  .then(function(data){
-  console.log(data);
+  data.results.forEach(function(user){
+    console.log(user);
+  })
+  console.log(data.results);
  })
 .catch(function(error){
   console.log(error);
